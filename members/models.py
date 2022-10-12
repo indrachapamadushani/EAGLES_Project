ba72bb988962243794aab5f784a993bbba9c8525
fsarahg <112925974+fsarahg@users.noreply.github.com>
2022-10-12 18:32:08 +0530

    Update models.py
    Created a Class Posts

--- members/models.py
+++ members/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid
 
 # Create your models here.
 # Defining an user profile 
@@ -12,3 +13,18 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.user.username
+ 
+# Class for Uploading Posts#
+class Posts(models.Model):
+    id = models.UUIDFIELD(primary_key= True, default =uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    no_of_likes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.use
+    
+    
+    
+
