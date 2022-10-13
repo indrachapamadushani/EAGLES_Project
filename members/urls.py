fc506c28f4120f802fb21d5ab2372de05bf153f2
aframbf <113178519+aframbf@users.noreply.github.com>
2022-10-13 21:35:09 +0530

    Update urls.py

--- members/urls.py
+++ members/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('settings', views.settings, name='settings'),
+    path('upload', views.upload, name='upload'),
+    path('follow', views.follow, name='follow'),
+    path('search', views.follow, name='follow'),
+    path('profile/<str:pk>', views.profile, name='profile'),
+    path('like-post', name='like-post'),
+    path('singup', views.singup, name='singup'),
+    path('signin', views.signin, name='signin'),
+    path('logout', views.logout, name='logout'),
 ]
