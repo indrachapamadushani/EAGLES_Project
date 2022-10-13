1a6dfdcfa075008c86e49d9cf10f1cbb12945cf9
aframbf <113178519+aframbf@users.noreply.github.com>
2022-10-13 21:48:36 +0530

    Update views.py

--- members/views.py
+++ members/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contib.auth.decorators import login_required
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+@login_required(login_url='signin')
+def search(request):
+    return render(request, 'serach.html')
+
+                
+
+
