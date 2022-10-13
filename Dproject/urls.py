10d75a9113945305e4c8ec88bce85e65ea48f792
aframbf <113178519+aframbf@users.noreply.github.com>
2022-10-13 21:30:18 +0530

    Update urls.py

--- Dproject/urls.py
+++ Dproject/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from.import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('members.urls')),
+    path('', views.index, name='index'),
+    path('settings', views.settings, name='settings'),
+    path('upload', views.upload, name='upload'),
+    path('follow', views.follow, name='follow'),
+    path('search', views.follow, name='follow'),
+    path('profile/<str:pk>', views.profile, name='profile'),
+    path('like-post', name='like-post'),
+    path('singup', views.singup, name='singup'),
+    path('signin', views.signin, name='signin'),
+    path('logout', views.logout, name='logout'),
 
 ]
