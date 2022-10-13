5c9f8d18433148a533e722a3f1769aa32013c291
fsarahg <112925974+fsarahg@users.noreply.github.com>
2022-10-13 16:12:08 +0530

    Update urls.py

--- members/urls.py
+++ members/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('settings', views.settings, name='settings'),
+    path('upload', views.upload, name='upload')
 ]
