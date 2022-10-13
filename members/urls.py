265e36b0b737ac9bbd63ea99a0a58b924a8d5bbf
indrachapa <chapamadushani99@gmail.com>
2022-10-13 18:01:47 +0530

    update urls.py

--- members/urls.py
+++ members/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('follow', views.follow, name='follow'),
+    path('profile/<str:pk>',views.profile,name='profile'),
+    path('like-post',views.like_post,name = 'like-post'),
 ]
