a5b1ef41d29f2e16dba1c6d4e2289c6ae23881e5
aframbf <113178519+aframbf@users.noreply.github.com>
2022-10-13 21:34:18 +0530

    Update urls.py

--- Dproject/urls.py
+++ Dproject/urls.py
@@ -18,15 +18,4 @@ from django.urls import path,include
 from.import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('settings', views.settings, name='settings'),
-    path('upload', views.upload, name='upload'),
-    path('follow', views.follow, name='follow'),
-    path('search', views.follow, name='follow'),
-    path('profile/<str:pk>', views.profile, name='profile'),
-    path('like-post', name='like-post'),
-    path('singup', views.singup, name='singup'),
-    path('signin', views.signin, name='signin'),
-    path('logout', views.logout, name='logout'),
-
-]
+   ]
