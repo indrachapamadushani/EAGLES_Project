ae70029f536a9055eafd8fbbe86687d6ef756153
ShanikaYodhagama <85022968+ShanikaYodhagama@users.noreply.github.com>
2022-10-13 11:36:28 -0700

    Template setup
    new four html files are added

--- members/views.py
+++ members/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>welcome to EAGLES project</h1>")
+    return render(request, 'index.html')
