39867648057f9db98a194cff6b4ff55a1fe5346d
aframbf <113178519+aframbf@users.noreply.github.com>
2022-10-13 22:13:45 +0530

    Update views.py

--- members/views.py
+++ members/views.py
@@ -9,7 +9,25 @@ def index(request):
 
 @login_required(login_url='signin')
 def search(request):
-    return render(request, 'serach.html')
+    user_object = user.objects.get(username=request.user.username)
+    user_profile = profile.objects.get(user=user_object)
+    
+    if request.method=='POST':
+        username = request.POST['username']
+        username_object = user_objects.filter(username__icontain=username)
+       
+        username_profile = []
+        username_profile_list = []
+        
+        for users in username_object:
+            username_profile.append(users.id)
+            
+        for ids in username_profile:
+            profile_lists = profile.objects.filter(id_user=ids)
+            username_profile_list.append(profile_lists)
+            
+       username_profile_list = list(chain("username_profile_list))   
+    return render(request, 'serach.html', {'user_profile': user_profile, 'username_profile_list': username_profile_list})
 
                 
 
