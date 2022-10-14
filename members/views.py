abe27c4c605c4bf2ed302d8565710b57c94803cb
indrachapa <chapamadushani99@gmail.com>
2022-10-14 20:30:04 +0530

    update views.py

--- members/views.py
+++ members/views.py
@@ -75,6 +75,27 @@ def upload(request):
     else:
         return redirect('/')
 
+
+@login_required(login_url='signin')
+def Like_Post(request)
+    username = request.user.username
+    post_id = request.GET.get('Post_id')
+
+    post = post.objects.get(id =post_id)
+
+    like_filter = LikePost.objects.filter(post_id = post_id, username = username).first()
+
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id = post_id, username = username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
 @login_required(login_url='signin')
 def search(request):
     user_object = User.objects.get(username=request.user.username)
