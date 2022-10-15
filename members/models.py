cb687b3791fb99f8c2a9efd3a071de3a58280cda
ShanikaYodhagama <85022968+ShanikaYodhagama@users.noreply.github.com>
2022-10-15 15:24:02 -0700

    user follow and unfollow setting

--- members/models.py
+++ members/models.py
@@ -36,6 +36,14 @@ class LikePost(models.Model):
     def __str__(self):
         return self.username
 
+
+class FollowersCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user
+
 # from django.db import models
 # from django.contrib.auth import get_user_model
 # import uuid
