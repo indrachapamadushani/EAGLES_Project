f9bece4a4528f8a8380b1cb1076d0de728629d8d
ShanikaYodhagama <85022968+ShanikaYodhagama@users.noreply.github.com>
2022-10-14 21:21:12 -0700

    User account setting

--- members/views.py
+++ members/views.py
@@ -2,12 +2,45 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 
 # Create your views here.
+
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
+
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+
+        if request.FILES.get('image') == None:
+            image= user_profile.profileimg
+            bio= request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location= location
+            user_profile.save()
+
+        if request.FILES.get('image')!= None:
+            image=request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        return redirect('settings')
+
+    return render(request, 'setting.html',{'user_profile':user_profile})
 
 
 def signup(request):
@@ -29,12 +62,15 @@ def signup(request):
                 user.save()
 
                 # log user in and redirect to setting page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
 
                 # create a profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
+
 
 
         else:
@@ -43,3 +79,27 @@ def signup(request):
 
     else:
         return render(request, 'signup.html')
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('/signin')
+
+    else:
+        return render(request, 'signin.html')
+
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
